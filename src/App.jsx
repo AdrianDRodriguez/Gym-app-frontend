39310b8ac945f086e4264979fd480963c3322b60
Adrian Rodriguez <ar9897.r@gmail.com>
2025-04-07 20:13:32 -0500

    Authentication and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
 import axios from "axios";
+import { createBrowserRouter, Outlet, Route, RouterProvider } from "react-router-dom";
 import { Header } from "./Header";
+import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
 import { ExercisesPage } from "./ExercisesPage";
 import { Footer } from "./Footer";
 
 axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
 
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header/>
+        <Outlet/>
+        <Footer/>
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <ExercisesPage/>,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage/>,
+      },
+      {
+        path: "/login",
+        element: <LoginPage/>,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <ExercisesPage />
-      <Footer />
-    </div>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App;
