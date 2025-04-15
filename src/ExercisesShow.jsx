620f278877965d5f069744afb664051069d5188e
Adrian Rodriguez <ar9897.r@gmail.com>
2025-04-14 21:53:27 -0500

    Exercise Modal design and button for add exercise

--- src/ExercisesShow.jsx
+++ src/ExercisesShow.jsx
@@ -4,19 +4,41 @@ export function ExercisesShow({ exercise }) {
   return (
     <div>
       <h1>Exercise Example</h1>
-      {youtubeId ? (
-        <iframe 
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${youtubeId}`}
-          title="Youtube video player" 
-          frameborder="0"
-          alllow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-        ) : (
-        <p>Invalid URL</p>
-      )}
+      <div style={{ display: "flex", gap: "20px" }} id="video">
+        {youtubeId ? (
+          <iframe 
+            width="1000"
+            height="300"
+            src={`https://www.youtube.com/embed/${youtubeId}`}
+            title="Youtube video player" 
+            frameborder="0"
+            alllow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          ) : (
+          <p>Invalid URL</p>
+        )}
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }} id="dropdown.and.button">
+
+        <select class="form-select form-select-sm" aria-label="Small select example">
+          <option selected>How many reps you want to add?</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+
+        <button type="button" class="btn btn-outline-dark">Add exercise</button>
+
+      </div>
+
     </div>
   );
 }
--- src/WodsIndex.jsx
+++ src/WodsIndex.jsx
@@ -1,18 +1,20 @@
 export function WodsIndex({ wods }) {
   return (
     <div>
-    <h1>All WODs</h1>
-    {wods ? (
-      wods.map((wod) => (
-        <div key={wod.id}>
-          <p>User:{wod.user_id}</p>
-          <p>Exercise:{wod.exercise_id}</p>
-          <p>Reps:{wod.reps}</p>
-        </div>
-      ))
-    ) : (
-      <p>Loading WODs...</p>
-    )}
+      <h1>All WODs</h1>
+        <table class="table table-striped-columns">
+          {wods ? (
+            wods.map((wod) => (
+              <div key={wod.id}>
+                <p>User: {wod.user_id}</p>
+                <p>Exercise: {wod.exercise}</p>
+                <p>Reps: {wod.reps}</p>
+              </div>
+            ))
+          ) : (
+            <p>Loading WODs...</p>
+          )}
+        </table>
   </div>
-);
+  );
 }
